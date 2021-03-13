84aa3d30b9e4d8a9a8ab781f1e7d7fffa7a6ff5a
Arifin Jiwantoko <jiwantoko@gmail.com>
2021-03-13 23:46:56 +0700

    Create resume page

--- README.md
+++ README.md
@@ -1,34 +1,11 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+## Greetings
+Hello, my name is Arifin Jiwantoko. This is my resume repository page, feel free to download if you like it. Don't forget to visit my personal website at http://jiwantoko.com. Interseted in hire me? send your email to arifin@jiwantoko.com. Thanks, best regards from me. 
+## Legal Alerts!
+The icon used for this page are listed bellow :
+[Email icon by Golmin Design](https://www.iconfinder.com/icons/1737384/e-mail_email_mail_message_send_icon)
+[Instagram icon Picons.me](https://www.iconfinder.com/icons/1161954/instagram_icon)
+[Github icon by Pixel Bazaar](https://www.iconfinder.com/icons/317712/code_repository_github_repository_resource_icon)
+[Heart Icon by Alexander Madyankin, Roman Shamin](https://www.iconfinder.com/icons/1814104/favorite_heart_like_love_icon)
 
-## Getting Started
-
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## Changelog
+Version 0.1 [March, 14 2021] : Create resume page.
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,145 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import 'normalize.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Jiwantoko</title>
       </Head>
+      <div className={styles.container}>
+        <div className={styles.main}>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          {/* profile */}
+          <div className={styles.profile}>
+            <img src="/profile.jpg" alt="profile picture"></img>
+            <h1>Hai, saya Arifin Jiwantoko</h1>
+            <p>Seorang antusias teknologi yang hobi mempelajari hal baru tentang perkembangan teknologi informasi dan memiliki keterampilan dalam bidang pengembangan web.</p>
+          </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          {/* skill */}
+          <div className={styles.skill}>
+            <h1>Skill</h1>
+              <h3>HTML</h3>
+              <div className={styles.skillBar}>
+                <div className={styles.skillHTML}>
+                  Advance
+                </div>
+              </div> 
+              <h3>CSS</h3>
+              <div className={styles.skillBar}>
+                <div className={styles.skillCSS}>
+                  Intermediate
+                </div>
+              </div> 
+              <h3>Javascript</h3>
+              <div className={styles.skillBar}>
+                <div className={styles.skillJavascript}>
+                  Intermediate
+                </div>
+              </div> 
+              <h3>PHP</h3>
+              <div className={styles.skillBar}>
+                <div className={styles.skillPHP}>
+                  Intermediate
+                </div>
+              </div> 
+              <h3>NodeJS</h3>
+              <div className={styles.skillBar}>
+                <div className={styles.skillNodeJS}>
+                  Beginner
+                </div>
+              </div> 
+              <h3>SQL</h3>
+              <div className={styles.skillBar}>
+                <div className={styles.skillSQL}>
+                  Advance
+                </div>
+              </div>
+              <h3>Sleep</h3>  
+              <div className={styles.skillBar}>
+                <div className={styles.skillSleep}>
+                  Expert
+                </div>
+              </div>  
+          </div>
+          
+          {/* project */}
+          <div className={styles.project}>
+            <h1>Project</h1>
+            <p>Will be update soon</p>
+          </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          {/* education and experience */}
+          <div className={styles.eductionExperience}>
+            <div className={styles.education}>
+              <h2>Pendidikan</h2>
+              <p><strong>Universitas Gunadarma</strong></p>
+              <p>S1 - Sistem Informasi</p>
+              <p>September 2013 - September 2017</p>
+              <p>IPK: 3.30</p>
+            </div>
+            <div className={styles.experience}>
+              <h2>Pengalaman</h2>
+              <p><strong>Usaha Street Food</strong></p>
+              <p>September 2017 - Sekarang</p>
+            </div>
+          </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          {/* contact */}
+          <div className={styles.contact}>
+            <div className={styles.contactHead}>
+              <h1>Contact</h1>
+              <p>Anda dapat menguhubungi saya melalui media dibawah.</p>
+            </div>
+            <div className={styles.contactLink}>
+              <div>
+                <img src="email.png" alt="email logo"></img>
+                <a href="mailto:arifin@jiwantoko.com">Arifin@jiwantoko.com</a>
+              </div>
+              <div>
+                <img src="instagram.png" alt="email logo"></img>
+                  <a href="https://www.instagram.com/jiwantoko/">@jiwantoko</a>
+              </div>
+              <div>
+                <img src="github.png" alt="email logo"></img>
+                <a href="https://github.com/jiwantoko">Jiwantoko</a>
+              </div>
+            </div>
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {/* contact form */}
+          <div className={styles.contactHead}>
+            <p>Atau isi form dibawah.</p>
+          </div>
+          
+          <div className={styles.contactForm}>
+            <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfka1KNFWDeiWD2BCCVomgZQf5kctpX-4YAcJzUvlBTg301Kw/formResponse" method="POST">
+              <div>
+                <label for="name">Nama</label>
+                <input type="text" name="entry.499303997" id="name" ></input>
+              </div>
+              <div>
+                <label for="email">Email</label>
+                <input type="text" name="entry.1097391147" id="email" ></input>
+              </div>
+              <div>
+                <label for="subject">Subject</label>
+                <input type="text" name="entry.1447426784" id="subject"></input>
+              </div>
+              <div>
+                <label for="pesan">Pesan</label>
+                <textarea name="entry.302030122"></textarea>
+              </div>
+                <input type="submit" name='kirim' value="Kirim"></input>
+            </form>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
+      <div className={styles.footer}>
+          Code with { <img src="/love.png" alt="love you.." className={styles.logo} /> } by{' '}Arifin Jiwantoko
+      </div>
     </div>
   )
 }
